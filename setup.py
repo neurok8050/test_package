6257272c2a9fad0f0aa512bebbecbb9c33fc9410
Claudéric <clauderic.deroy@umontreal.ca>
2022-07-19 11:28:56 -0400

    packaged it and added to test pypi

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(
+    name='test_package_DnD',
+    version='0.0.1',
+    author='Claudéric',
+    description='simple test packaging',
+    long_description='a simple test packaging that can be use for DnD',
+    url='https://github.com/neurok8050',
+    keywords='development, setup, setuptools',
+    python_requires='>=3.7, <4'
+)
